5bc59b8938b82080fdbd308972d3846bcfafc07f
Michaël Nass - PlugN <michael.nass@free.fr>
2022-06-26 14:55:25 +0200

    oops

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__).'../config/config.php';
+require_once dirname(__FILE__).'/../config/config.php';
 require_once dirname(__FILE__).'/../config/efreidynconfig.php';
 
 require 'PHPMailer/PHPMailer.php';
