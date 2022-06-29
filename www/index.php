b24aff2130c749bc06196ee2da9241c89a163386
julienMabecque <julien.mabecque@efrei.net>
2022-06-29 11:26:51 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -124,7 +124,7 @@ if (isset($_SESSION['id'])){
 
                         echo '
                         <div class="alert alert-info fade show" role="alert">
-                          <strong>Voici votre token :', $token ,'qu\'il faudra présenter dans la mairie de votre choix.<br>
+                          <strong>Voici votre token [', $token ,'] <br>Ce dernier devra être présenté dans la mairie de votre choix.<br>
                         </div>';
                     
 
