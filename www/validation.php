ea91f8eb9652e8f54370b29dd4ed2eed7d89dc6c
julienMabecque <julien.mabecque@efrei.net>
2022-06-29 11:04:01 +0200

    correction

--- www/validation.php
+++ www/validation.php
@@ -46,7 +46,7 @@ if (isset($_SESSION['id'])){
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="https://www.intellivote.fr">Espace élécteur<span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="https://www.intellivote.fr">Espace électeur<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="https://mairie.intellivote.fr">Espace mairie</a>
