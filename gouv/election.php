9946e8571e8c0aa6ad6f8b048eb66e37889d711a
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-07-04 16:42:44 +0200

    clean

--- gouv/election.php
+++ gouv/election.php
@@ -3,126 +3,7 @@ require_once dirname(__FILE__).'/../config.php';
 
 
 if (isset($_SESSION['id'])){
-    if (!isset($_GET['ajout']) AND !isset($_GET['descriptionerror']) AND !isset($_GET['beginerror']) AND !isset($_GET['enderror'])){
-        echo '<!DOCTYPE html>
-        <html lang="fr">
-
-        <head>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <meta http-equiv="Content-Security-Policy" content="default-src \'self\'; img-src https://* \'self\' data:; style-src https://* \'self\' \'unsafe-inline\' child-src \'none\';">
-
-        <title>Intellivote - Espace Gouvernement - Election</title>
-
-        <link href="css/custom.css" rel="stylesheet">
-
-        <!-- Bootstrap core CSS -->
-        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="css/blog-home.css" rel="stylesheet">
-
-        </head>
-
-        <body>
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger">
-            <div class="container">
-            <a class="navbar-brand" href="index.php"><img src="image/logo.png" width="160" height="30"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span id="new-dark-navbar-toggler-icon" class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="https://www.intellivote.fr">Espace électeur</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://mairie.intellivote.fr">Espace mairie</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="https://gouv.intellivote.fr">Espace Gouvernement<span class="sr-only">(current)</span></a>
-                </li>';
-
-                echo '
-                </ul>
-            </div>
-            </div>
-        </nav>
-
-        <!-- Page Content -->
-        <div class="container">
-
-            <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">';
-
-                echo '<h1 class="my-4">Espace Gouvernement - Election</h1>';
-
-                $gouv_fetch = $bdd->prepare('SELECT * FROM governor WHERE individual = ? AND verified = 1;');
-                $gouv_fetch->execute(array($_SESSION['id']));
-                $gouv = $gouv_fetch->fetch();
-
-                if (!$gouv) {
-                echo '<div class="alert alert-danger fade show" role="alert">
-                    <strong>L\'espace Gouvernement n\'est pas accessible depuis l\'extérieur.</strong> Par sécurité, vous devez utiliser l\'interface de gestion interne d\'Intellivote pour pouvoir administrer le service, la connexion à distance n\'est pas possible. Intellivote ne vous demandera jamais vos identifiants ni codes de vérifications, ne les communiquez jamais.
-                </div><br><br>';
-                } else {
-                    if (!isset($_GET['verify'])){
-
-                        /*if (isset($_GET['success'])) {
-                        echo '
-                        <div class="alert alert-success fade show" role="alert">
-                            <strong>LE maire a bien été affilié à la mairie.</strong>
-                        </div>';
-                        }*/
-
-                        echo '
-                        <h2><a>Afficher une élection :</a></h2>
-                        <br><br>';
-                    }
-
-                    echo '
-                    <a class = "btn btn-secondary" href = "index.php">Retour</a>
-                    <br><br>';
-
-                }
-
-                echo '
-                <a class = "btn btn-secondary" href = "logout.php">Se déconnecter</a>
-                <br><br>';
-
-            echo '</div>
-
-            </div>
-            <!-- /.row -->
-
-        </div>
-        <!-- /.container -->
-
-        <!-- Footer -->
-        <footer class="py-5" style="background-color: #e04a51;">
-            <div class="container">
-            <p class="m-0 text-center text-white">&copy; 2022 Intellivote. Tous droits reservés. <a href="https://www.intellivote.fr/legal.php" style="color: lightcyan;">Mentions légales</a>.</p>
-            </div>
-            <!-- /.container -->
-        </footer>
-
-        <!-- Bootstrap core JavaScript -->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        </body>
-
-        </html>';
-    }
-    else if(!isset($_POST['description'])){
+    if (!isset($_GET['verify'])){
 
         echo '<!DOCTYPE html>
         <html lang="fr">
@@ -194,14 +75,7 @@ if (isset($_SESSION['id'])){
                     <strong>L\'espace Gouvernement n\'est pas accessible depuis l\'extérieur.</strong> Par sécurité, vous devez utiliser l\'interface de gestion interne d\'Intellivote pour pouvoir administrer le service, la connexion à distance n\'est pas possible. Intellivote ne vous demandera jamais vos identifiants ni codes de vérifications, ne les communiquez jamais.
                 </div><br><br>';
                 } else {
-                    if (!isset($_GET['verify'])){
-
-                        /*if (isset($_GET['success'])) {
-                        echo '
-                        <div class="alert alert-success fade show" role="alert">
-                            <strong>LE maire a bien été affilié à la mairie.</strong>
-                        </div>';
-                        }*/
+                    if (isset($_GET['ajout']) OR isset($_GET['descriptionerror']) OR isset($_GET['beginerror']) OR isset($_GET['enderror'])){
 
                         echo '
                         <h2><a>Ajouter une élection :</a></h2>
@@ -250,7 +124,13 @@ if (isset($_SESSION['id'])){
                             <button type="submit" class="btn btn-primary">Créer l\'élection</button>
 
                         </form><br><br>';
+
+                    } else {
+                        echo '
+                        <h2><a>Afficher une élection</a></h2>';
                     }
+
+                    
 
                     echo '
                     <a class = "btn btn-secondary" href = "index.php">Retour</a>
