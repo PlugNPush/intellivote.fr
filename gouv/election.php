8c0fdca2a185cfde6d928332920f435333381abb
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-07-04 14:42:02 +0200

    election form

--- gouv/election.php
+++ gouv/election.php
@@ -3,229 +3,205 @@ require_once dirname(__FILE__).'/../config.php';
 
 
 if (isset($_SESSION['id'])){
+    if(!isset($_POST['description'])){
 
-    echo '<!DOCTYPE html>
-    <html lang="fr">
+        echo '<!DOCTYPE html>
+        <html lang="fr">
 
-    <head>
+        <head>
 
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="">
-      <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-      <meta http-equiv="Content-Security-Policy" content="default-src \'self\'; img-src https://* \'self\' data:; style-src https://* \'self\' \'unsafe-inline\' child-src \'none\';">
+        <meta http-equiv="Content-Security-Policy" content="default-src \'self\'; img-src https://* \'self\' data:; style-src https://* \'self\' \'unsafe-inline\' child-src \'none\';">
 
-      <title>Intellivote - Espace Gouvernement - Election</title>
+        <title>Intellivote - Espace Gouvernement - Election</title>
 
-      <link href="css/custom.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
 
-  <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-      <!-- Custom styles for this template -->
-      <link href="css/blog-home.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/blog-home.css" rel="stylesheet">
 
-    </head>
+        </head>
 
-    <body>
+        <body>
 
-      <!-- Navigation -->
-      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger">
-        <div class="container">
-          <a class="navbar-brand" href="index.php"><img src="image/logo.png" width="160" height="30"></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span id="new-dark-navbar-toggler-icon" class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="https://www.intellivote.fr">Espace électeur</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://mairie.intellivote.fr">Espace mairie</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="https://gouv.intellivote.fr">Espace Gouvernement<span class="sr-only">(current)</span></a>
-              </li>';
-
-              echo '
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <!-- Page Content -->
-      <div class="container">
-
-        <div class="row">
-
-          <!-- Blog Entries Column -->
-          <div class="col-md-8">';
-
-            echo '<h1 class="my-4">Espace Gouvernement - Election</h1>';
-
-            $gouv_fetch = $bdd->prepare('SELECT * FROM governor WHERE individual = ? AND verified = 1;');
-            $gouv_fetch->execute(array($_SESSION['id']));
-            $gouv = $gouv_fetch->fetch();
-
-            if (!$gouv) {
-              echo '<div class="alert alert-danger fade show" role="alert">
-                <strong>L\'espace Gouvernement n\'est pas accessible depuis l\'extérieur.</strong> Par sécurité, vous devez utiliser l\'interface de gestion interne d\'Intellivote pour pouvoir administrer le service, la connexion à distance n\'est pas possible. Intellivote ne vous demandera jamais vos identifiants ni codes de vérifications, ne les communiquez jamais.
-              </div><br><br>';
-            } else {
-              if (!isset($_GET['verify'])){
-
-                /*if (isset($_GET['success'])) {
-                  echo '
-                  <div class="alert alert-success fade show" role="alert">
-                    <strong>LE maire a bien été affilié à la mairie.</strong>
-                  </div>';
-                }*/
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger">
+            <div class="container">
+            <a class="navbar-brand" href="index.php"><img src="image/logo.png" width="160" height="30"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span id="new-dark-navbar-toggler-icon" class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.intellivote.fr">Espace électeur</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://mairie.intellivote.fr">Espace mairie</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="https://gouv.intellivote.fr">Espace Gouvernement<span class="sr-only">(current)</span></a>
+                </li>';
 
                 echo '
-                  <h2><a>Ajouter une élection :</a></h2>
-                  <form action="election.php" method="post">
+                </ul>
+            </div>
+            </div>
+        </nav>
 
-                    <div class="form-group">
-                      <label for="name_election">Saisissez le nom de l\'élection</label>
-                      <input type="text" name="name_election" class="form-control';
+        <!-- Page Content -->
+        <div class="container">
 
-                      if (isset($_GET['nameerror'])){
-                        echo ' is-invalid';
-                      }
+            <div class="row">
 
-                      echo ' "id="name_election" placeholder="Saisissez le nom de l\'élection" required> ';
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">';
 
-                      if (isset($_GET['nameerror'])){
-                        echo '<div class="invalid-feedback">
-                          Nom de l\'élection incorrect ! Une élection à ce nom est déjà en cours.
+                echo '<h1 class="my-4">Espace Gouvernement - Election</h1>';
+
+                $gouv_fetch = $bdd->prepare('SELECT * FROM governor WHERE individual = ? AND verified = 1;');
+                $gouv_fetch->execute(array($_SESSION['id']));
+                $gouv = $gouv_fetch->fetch();
+
+                if (!$gouv) {
+                echo '<div class="alert alert-danger fade show" role="alert">
+                    <strong>L\'espace Gouvernement n\'est pas accessible depuis l\'extérieur.</strong> Par sécurité, vous devez utiliser l\'interface de gestion interne d\'Intellivote pour pouvoir administrer le service, la connexion à distance n\'est pas possible. Intellivote ne vous demandera jamais vos identifiants ni codes de vérifications, ne les communiquez jamais.
+                </div><br><br>';
+                } else {
+                    if (!isset($_GET['verify'])){
+
+                        /*if (isset($_GET['success'])) {
+                        echo '
+                        <div class="alert alert-success fade show" role="alert">
+                            <strong>LE maire a bien été affilié à la mairie.</strong>
                         </div>';
-                      }
+                        }*/
 
-                      echo ' <small id="IDHelp" class="form-text text-muted">
-                        Vous ne pouvez pas utiliser le nom d\'une élection déjà en cours.
-                      </small>
+                        echo '
+                        <h2><a>Ajouter une élection :</a></h2>
+                        <form action="election.php" method="post">
 
-                      <label for="dates">Choisissez les dates de l\'élection</label>
+                            <div class="form-group">
+                                <label for="description">Saisissez le nom de l\'élection</label>
+                                <input type="text" name="description" class="form-control';
 
-                      <input type="date" name="begindate" class="form-control';
-                      if (isset($_GET['beginerror'])){
-                        echo ' is-invalid';
-                      }
-                      echo '" id="begindate" placeholder="Saisissez la date de début." required>
+                                if (isset($_GET['descriptionerror'])){
+                                    echo ' is-invalid';
+                                }
 
-                      <input type="date" name="enddate" class="form-control';
-                      if (isset($_GET['enderror'])){
-                        echo ' is-invalid';
-                      }
-                      echo '" id="enddate" placeholder="Saisissez la date de fin." required>
+                                echo ' "id="description" placeholder="Saisissez le nom de l\'élection" required> ';
 
-                      <label for="description">Saisissez une description</label>
-                      <input type="text" name="description" class="form-control';
+                                if (isset($_GET['descriptionerror'])){
+                                    echo '<div class="invalid-feedback">
+                                    Nom de l\'élection incorrect ! Une élection à ce nom est déjà en cours.
+                                    </div>';
+                                }
 
-                      if (isset($_GET['descriptionerror'])){
-                        echo ' is-invalid';
-                      }
+                                echo ' <small id="NameHelp" class="form-text text-muted">
+                                    Vous ne pouvez pas utiliser le nom d\'une élection déjà en cours.
+                                </small>
 
-                      echo ' "id="description" placeholder="Saisissez une description" required> ';
+                                <label for="dates">Choisissez les dates de l\'élection</label>
 
-                      if (isset($_GET['descriptionerror'])){
-                        echo '<div class="invalid-feedback">
-                          La description est trop longue ! Vous n\'avez le droit qu\'à 255 caractères.
-                        </div>';
-                      }
+                                <input type="date" name="begindate" class="form-control';
+                                if (isset($_GET['beginerror'])){
+                                    echo ' is-invalid';
+                                }
+                                echo '" id="begindate" placeholder="Saisissez la date de début." required>
+
+                                <input type="date" name="enddate" class="form-control';
+                                if (isset($_GET['enderror'])){
+                                    echo ' is-invalid';
+                                }
+                                echo '" id="enddate" placeholder="Saisissez la date de fin." required>
+
+                                <small id="DateHelp" class="form-text text-muted">
+                                    Date de début qu\'à partir de demain, et date de fin qu\'à partir de la date de début.
+                                </small>
+
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Créer l\'élection</button>
+
+                        </form><br><br>';
+                    }
 
                     echo '
-                    </div>
+                    <a class = "btn btn-secondary" href = "index.php">Retour</a>
+                    <br><br>';
 
-                    <button type="submit" class="btn btn-primary">Créer l\'élection</button>
-
-                  </form><br><br>';
-              } else {
+                }
 
                 echo '
-                  <h2><a>Vérification :</a></h2>
-                  <form action="index.php" method="post">
+                <a class = "btn btn-secondary" href = "logout.php">Se déconnecter</a>
+                <br><br>';
 
-                    <div class="form-group">
-                      <label for="name_election">Confirmez vous les données ?</label>
-                      <input type="hidden" name="name_election" class="form-control';
+            echo '</div>
 
-                      if (isset($_GET['nameerror'])){
-                        echo ' is-invalid';
-                      }
-                      
-                      echo '" id="name_election" value="';echo(array($_POST['name_election']));echo'" required>';
+            </div>
+            <!-- /.row -->
 
-                      if (isset($_GET['nameerror'])){
-                        echo '<div class="invalid-feedback">
-                          ID du maire incorrect ! Besoin d\'aide ? Contactez l\'électeur afin de vérifier que l\'ID soit correct.
-                        </div>';
-                      }
-
-                      echo '<input type="hidden" name="idmairie" class="form-control';
-
-                      if (isset($_GET['nameerror'])){
-                        echo ' is-invalid';
-                      }
-                      
-                      echo '" id="idmairie" value="';echo(array($_POST['idmairie']));echo'" required>';
-
-                      if (isset($_GET['inameerror'])){
-                        echo '<div class="invalid-feedback">
-                          ID du maire incorrect ! Besoin d\'aide ? Contactez l\'électeur afin de vérifier que l\'ID soit correct.
-                        </div>';
-                      }
-                      
-                    echo '</div>
-
-                    <button type="submit" class="btn btn-primary">Confirmer les données</button>
-
-                  </form><br>
-
-                  <form action="index.php" method="post">
-
-                    <button type="submit" class="btn btn-primary">Retour en arrière</button>
-
-                  </form><br><br>';
-
-              }
-
-              echo '
-              <a class = "btn btn-secondary" href = "index.php">Retour</a>
-              <br><br>';
-
-            }
-
-            echo '
-            <a class = "btn btn-secondary" href = "logout.php">Se déconnecter</a>
-            <br><br>';
-
-          echo '</div>
-
-        </div>
-        <!-- /.row -->
-
-      </div>
-      <!-- /.container -->
-
-      <!-- Footer -->
-      <footer class="py-5" style="background-color: #e04a51;">
-        <div class="container">
-          <p class="m-0 text-center text-white">&copy; 2022 Intellivote. Tous droits reservés. <a href="https://www.intellivote.fr/legal.php" style="color: lightcyan;">Mentions légales</a>.</p>
         </div>
         <!-- /.container -->
-      </footer>
 
-      <!-- Bootstrap core JavaScript -->
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Footer -->
+        <footer class="py-5" style="background-color: #e04a51;">
+            <div class="container">
+            <p class="m-0 text-center text-white">&copy; 2022 Intellivote. Tous droits reservés. <a href="https://www.intellivote.fr/legal.php" style="color: lightcyan;">Mentions légales</a>.</p>
+            </div>
+            <!-- /.container -->
+        </footer>
 
-    </body>
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    </html>';
+        </body>
 
+        </html>';
+
+    } else {
+
+        // Pas de description/nom en double parmis ceux non finis
+        $req = $bdd->prepare('SELECT * FROM election WHERE description = ?;');
+        $req->execute(array($_POST['description']));
+        $test = $req->fetch();
+    
+        
+        $req = $bdd->prepare('SELECT * FROM election WHERE id = ?;');
+        $req->execute(array($_POST['begindate']));
+        $test2 = $req->fetch();
+    
+        if ($test){
+          header( "refresh:0;url=election.php?descriptionerror=true" );
+        }
+        else if ($_POST['begindate']<=date('Y/m/d')){ // Date de début qu'à partir de demain
+          header( "refresh:0;url=election.php?beginerror=true" );
+        }
+        else if ($_POST['begindate']>$_POST['enddate']){ // Date de fin qu'à partir de la date de début
+            header( "refresh:0;url=election.php?enderror=true" );
+        }
+        else{
+    
+          $req=$bdd->prepare('INSERT INTO election (description, begindate, enddate) VALUES (:description, :begindate, :enddate);');
+          $req->execute(array(
+            'description'=> $_POST['description'],
+            'begindate'=> $_POST['begindate'],
+            'enddate'=> $_POST['enddate']
+          ));
+    
+          header( "refresh:0;url=index.php?successelection=true" );
+          
+        }
+      
+    }
 
 } else {
   header( "refresh:0;url=login.php?expired=true" );
