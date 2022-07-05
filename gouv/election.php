520e72a3ab5b705167eddc32e511a4b86ffd9944
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-07-05 18:25:55 +0200

    mb

--- gouv/election.php
+++ gouv/election.php
@@ -138,7 +138,7 @@ if (isset($_SESSION['id'])){
                         echo '
                         <h2><a>Ajouter un candidat</a></h2>';
                         echo '
-                        <form action="election.php?ajoutcandidat=true" method="post">
+                        <form action="election.php?" method="post">
                             <div class="form-group">
                                 <label for="name">Nom</label>
                                 <input type="text" name="name" class="form-control';
