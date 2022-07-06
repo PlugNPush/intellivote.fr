f5df0c96510d8df4359acdd43e177f54297ea460
Michaël Nass - PlugN <michael.nass@free.fr>
2022-07-06 23:02:23 +0200

    test

--- www/index.php
+++ www/index.php
@@ -637,6 +637,8 @@ if (isset($_SESSION['id'])){
 
 
         $verify = password_verify($_POST['omdp'], $test['password']);
+
+        echo $verify . $test['password'] . $_POST['omdp'] . $pass_hache . $_POST['mdp']
         if ($verify)
         {
           $upd1 = $bdd->prepare('UPDATE individual SET password = ? WHERE individual = ?;');
