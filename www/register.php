344e42211ac3e51f2ed0b219648654c1eacda553
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-07-06 21:02:48 +0200

    test

--- www/register.php
+++ www/register.php
@@ -174,7 +174,7 @@ if(empty($_POST['mdp']) OR empty($_POST['vmdp'])){
 
             <div class="form-group">
               <label for="titre">Date de naissance</label>
-              <input type="datetime" name="birthdate" class="form-control';
+              <input type="date" name="birthdate" class="form-control';
               if (isset($_GET['birthdateerror'])){
                 echo ' is-invalid';
               }
